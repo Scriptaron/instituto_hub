73c0ce095e60c4f9758fbad45578f9f69ea09335
Scriptaron <aaron.manzone@gmail.com>
2026-03-13 17:15:28 -0300

    feat: integra sanity cms e cria subdiretório do stúdio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sanity({
+    projectId: 'btt36vts',
+    dataset: 'production',
+    useCdn: false, // usa CDN só em produção pra economizar requests, falso em dev
+    apiVersion: '2023-05-03',
+  })]
 });
