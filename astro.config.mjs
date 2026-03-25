e293039e4d03ab8d250840c128c2afdae374aa57
Scriptaron <aaron.manzone@gmail.com>
2026-03-25 16:16:05 -0300

    refactor: otimização do SSR e correção de dependências do ambiente dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sanity from '@sanity/astro';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   },
   integrations: [sanity({
     projectId: 'btt36vts',
     dataset: 'production',
-    useCdn: false, // usa CDN só em produção pra economizar requests, falso em dev
+    useCdn: process.env.NODE_ENV === 'production',
     apiVersion: '2023-05-03',
   })]
 });
